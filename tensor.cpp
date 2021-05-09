fed9a160899a658be0029eb18fd8d7bf9cd7dbe1
Nicola Marizza <887004@stud.unive.it>
2021-05-09 18:04:28 +0200

    Operators, utilities
    data è un array monodimensionale a cui accedi tramite una mappa di memorizzazione (operator())

--- tensor.cpp
+++ tensor.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <math.h>
 #include <fstream>
+#include <string.h>
 
 #include "dais_exc.h"
 #include "tensor.h"
@@ -39,4 +40,227 @@ void Tensor::init_random(float mean, float std){
     }else{
         throw(tensor_not_initialized());
     }
+}
+
+Tensor::Tensor(int r, int c, int d, float v)
+{
+	this->r = r;
+	this->c = c;
+	this->d = d;
+	data = new float[r * c * d];
+	if(v)
+		init(r, c, d, v);
+}
+
+Tensor::Tensor(const Tensor& that)
+{
+	r = that.r;
+	c = that.c;
+	d = that.d;
+
+	int size = r * c * d;
+	data = new float[size];
+	memcpy(data, that.data, size * sizeof(float));
+}
+
+Tensor::~Tensor()
+{
+	delete[] data;
+	data = nullptr;
+}
+
+float Tensor::operator()(int i, int j, int k) const
+{
+	if(!data)
+		throw(index_out_of_bound());
+
+	return data[(i * c * d) + (j * d) + k];
+}
+
+float& Tensor::operator()(int i, int j, int k)
+{
+	if(!data)
+		throw(index_out_of_bound());
+
+	return data[(i * c * d) + (j * d) + k];
+}
+
+Tensor Tensor::operator-(const Tensor &rhs)
+{
+	if(r != rhs.r || c != rhs.c || d != rhs.d)
+		throw(dimension_mismatch());
+
+	Tensor result = Tensor(*this);
+
+	int size = r * c * d;
+	for(int n = 0; n < size; n++)
+		result.data[n] -= rhs.data[n];
+
+	return result;
+}
+
+Tensor Tensor::operator+(const Tensor &rhs)
+{
+	if(r != rhs.r || c != rhs.c || d != rhs.d)
+		throw(dimension_mismatch());
+
+	Tensor result = Tensor(*this);
+
+	int size = r * c * d;
+	for(int n = 0; n < size; n++)
+		result.data[n] += rhs.data[n];
+
+	return result;
+}
+
+Tensor Tensor::operator*(const Tensor &rhs)
+{
+	if(r != rhs.r || c != rhs.c || d != rhs.d)
+		throw(dimension_mismatch());
+		
+	Tensor result = Tensor(*this);
+
+	int size = r * c * d;
+	for(int n = 0; n < size; n++)
+		result.data[n] *= rhs.data[n];
+
+	return result;
+}
+
+Tensor Tensor::operator/(const Tensor &rhs)
+{
+	if(r != rhs.r || c != rhs.c || d != rhs.d)
+		throw(dimension_mismatch());
+		
+	Tensor result = Tensor(*this);
+
+	int size = r * c * d;
+	for(int n = 0; n < size; n++)
+		result.data[n] /= rhs.data[n];
+
+	return result;
+}
+
+Tensor Tensor::operator-(const float &rhs)
+{
+	Tensor result = Tensor(*this);
+
+	int size = r * c * d;
+	for(int n = 0; n < size; n++)
+		result.data[n] -= rhs;
+
+	return result;
+}
+
+Tensor Tensor::operator+(const float &rhs)
+{
+	Tensor result = Tensor(*this);
+
+	int size = r * c * d;
+	for(int n = 0; n < size; n++)
+		result.data[n] += rhs;
+
+	return result;
+}
+
+Tensor Tensor::operator*(const float &rhs)
+{
+	Tensor result = Tensor(*this);
+
+	int size = r * c * d;
+	for(int n = 0; n < size; n++)
+		result.data[n] *= rhs;
+
+	return result;
+}
+
+Tensor Tensor::operator/(const float &rhs)
+{
+	Tensor result = Tensor(*this);
+
+	int size = r * c * d;
+	for(int n = 0; n < size; n++)
+		result.data[n] /= rhs;
+
+	return result;
+}
+
+Tensor& Tensor::operator=(const Tensor &other)
+{
+	r = other.r;
+	c = other.c;
+	d = other.d;
+
+	int size = r * c * d;
+	delete[] data;
+	data = new float[size];
+	memcpy(data, other.data, size * sizeof(float));
+
+	return *this;
+}
+
+void Tensor::init(int r, int c, int d, float v)
+{
+	int size = r * c * d;
+	for(int n = 0; n < size; n++)
+		data[n] = v;
+}
+
+void Tensor::clamp(float low, float high)
+{
+	int size = r * c * d;
+	for(int n = 0; n < size; n++)
+		if(data[n] < low)
+			data[n] = low;
+		else if(data[n] > high)
+			data[n] = high;
+}
+
+
+int Tensor::rows()
+{
+	return r;
+}
+
+int Tensor::cols()
+{
+	return c;
+}
+
+int Tensor::depth()
+{
+	return d;
+}
+
+float Tensor::getMin(int k)
+{
+	float min {operator()(0, 0, k)};
+	for(int i = 0; i < r; i++)
+		for(int j = 1; j < c; j++)
+		{
+			float val = operator()(i, j, k);
+			if(val < min)
+				min = val;
+		}
+
+	return min;
+}
+
+float Tensor::getMax(int k)
+{
+	float max {operator()(0, 0, k)};
+	for(int i = 0; i < r; i++)
+		for(int j = 1; j < c; j++)
+		{
+			float val = operator()(i, j, k);
+			if(val > max)
+				max = val;
+		}
+
+	return max;
+}
+
+void Tensor::showSize()
+{
+	cout << r << " x " << c << " x " << d << endl;
 }
