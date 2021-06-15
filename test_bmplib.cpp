b5b1b3fd830515d04cc374ba84ac14a5610831a0
Nicola Marizza <887004@stud.unive.it>
2021-06-15 09:40:09 +0200

    Added prof changes

--- test_bmplib.cpp
+++ test_bmplib.cpp
@@ -25,36 +25,7 @@ void generate_checkboard(int w=512, int h=512, string filename="checkboard.bmp")
 	img.write (filename.c_str());
 }
 
-void  generate_italia(int w=512, int h=512, string filename="italia.bmp")
-{
-	BmpImg img (w, h);
-
-	// Draw the italian flag:
-
-	for (int x = 0; x < w; x++)
-	{
-		for (int y = 0; y < h; y++)
-		{
-			if (x < w/3)
-			{
-				img.set_pixel (x, y, 0, 140, 69);
-			}
-			else if (x < w*2/3)
-			{
-				img.set_pixel (x, y, 244, 249, 255);
-			}
-			else
-			{
-				img.set_pixel (x, y, 205, 33, 42);
-			}
-		}
-	}
-
-	img.write (filename.c_str());
-}
-
 int main(){
     generate_checkboard(512,512,"checkboard.bmp");
-	generate_italia(512,342,"italia.bmp");
     return 0;
 }
